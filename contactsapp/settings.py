32f4a4359021d094c0d60487ce2a49f64e2df18d
Akinwole Adam <akinwoleadam@gmail.com>
2022-09-21 16:23:56 +0100

    deploying on railway

--- contactsapp/settings.py
+++ contactsapp/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['web-production-7db1.up.railway.app','127.0.0.1']
 
-CSRF_TRUSTED_ORIGINS = ['https://web-production-7db1.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-7db1.up.railway.app']
 
 # Application definition
 
